c8834796b5d786173d993778e0144cb6d59fd84b
Jame <jaimehgb@gmail.com>
2018-05-07 23:48:41 +0200

    Authorized ips mail initial commit

---
+++ app/Mail/AuthorizeIpMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AuthorizeIpMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($ip)
+    {
+        $this->ip = $ip;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.ipAuth')
+                    ->from('support@nanowallet.io', 'NanoWallet Support')
+                    ->subject('Authorize new IP')
+                    ->with(['ipAttempt' => $this->ip]);
+    }
+}

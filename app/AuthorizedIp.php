50dc53dd112651c2bab90c2cfb1353f8e4f56a38
Jame <jaimehgb@gmail.com>
2018-05-08 02:59:05 +0200

    AuthorizedIp::create()

--- app/AuthorizedIp.php
+++ app/AuthorizedIp.php
@@ -16,4 +16,29 @@ class AuthorizedIp extends Model
     		return true;
     	return false;
     }
+
+    public static function create (\Illuminate\Http\Request $request, $wallet_id)
+    {
+    	// check if exists already
+    	$aip = AuthorizedIp::where('wallet_id', $wallet_id)->where('ip', $request->ip())->first();
+    	if ($aip)
+    	{
+    		if ($aip->authorized)
+    			return false;
+    		else
+    			return $aip;;
+    	}
+    	else
+    	{
+    		$aip = new AuthorizedIp();
+    		$aip->ip = $request->ip();
+    		$aip->wallet_id = $wallet_id;
+    		$aip->authorized = false;
+    		$aip->rand_id = hash('sha256', $wallet_id . $request->ip() . time() . rand(0, 100000));
+    		$aip->expires = time() + 3600 * 2;
+    		$aip->info = $request->header('User-Agent');
+    		$aip->save();
+    		return $aip;
+    	}
+    }
 }

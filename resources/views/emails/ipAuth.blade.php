e18163ea06690314bd0a693e21ac904fd4882510
Miquel Larsson <miquellarsson@gmail.com>
2018-05-08 03:21:39 +0200

    Small auth ip email fixes

--- resources/views/emails/ipAuth.blade.php
+++ resources/views/emails/ipAuth.blade.php
@@ -5,11 +5,11 @@
     <div style="width:100%; height:100%; margin:0; padding:0">
       <h1>
         Authorize a new IP address to log in to your wallet.
-      </h1>
+      </h1><br/>
       <p style="text-align:left">
         Hi there, <br/><br/>
 
-        it seems you are trying to log in with a new IP address. You can authorize it at the link below:<br/><br/>
+        It seems that you are trying to log in to your wallet with a new IP address. You can authorize it with the link below:<br/><br/>
         
         <a href="https://ipauth.nanowallet.io/{{ $authorizedIpModel->rand_id }}">https://ipauth.nanowallet.io/{{ $authorizedIpModel->rand_id }}</a>
         <br/><br/>
@@ -17,7 +17,7 @@
         <br/><br/>
         If you need anything you can contact us at support@nanowallet.io.<br/><br/>
         Sincerely,<br/>
-        NanoWallet.io Team.
+        The NanoWallet.io Team.
       </p>
     </div>
   </body>
